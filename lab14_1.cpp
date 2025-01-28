2151d9c1d4c87e3a567e76417f25f9ad072b0b04
Champ <champsrw49@gmail.com>
2025-01-28 16:55:22 +0700

    Lab14-1 complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[],int N){
-	for (int i = 1; i < N; i ++){
-		int num = d[i];
-		int j = i - 1;
+void insertionSort(T d[], int N) {
+    for (int i = 1; i < N; i++) {
+        T num = d[i];
+        int j = i - 1;
 
-		while (j >= 0 && d[j] > num){
-			d[j + 1] = d[j];
-			j--;
-		}
-		d[j + 1] = num;
-	}
+        while (j >= 0 && d[j] < num) {
+            d[j + 1] = d[j];
+            j--;
+        }
+        d[j + 1] = num;
+        
+         cout << "Pass " << i << ":";
+        for (int k = 0; k < N; k++) {
+            cout << d[k] << " ";
+        }
+        cout << "\n";
+    }
 }
+
 
 
 int main(){
