75ee440f0757ae60de4515dcce7127909a9f4a25
Champ <champsrw49@gmail.com>
2025-01-28 16:50:17 +0700

    Lab14-2 complete

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -3,8 +3,18 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
+	for (int i = 1; i < N; i ++){
+		int num = d[i];
+		int j = i - 1;
 
+		while (j >= 0 && d[j] > num){
+			d[j + 1] = d[j];
+			j--;
+		}
+		d[j + 1] = num;
+	}
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
--- lab14_2.cpp
+++ lab14_2.cpp
@@ -3,21 +3,51 @@ using namespace std;
 
 const int N = 5;
 
-void inputMatrix(double [][N]);
-
-void findLocalMax(const double [][N], bool [][N]);
-
-void showMatrix(const bool [][N]);
-
-int main(){
-	double A[N][N]; 
-	bool B[N][N]; 
-	cout << "Input the matrix.....\n";
-	inputMatrix(A);
-	findLocalMax(A,B);
-	cout << "Result is \n";
-	showMatrix(B);
-	return 0;
+void inputMatrix(double matrix[][N]) {
+    for (int i = 0; i < N; i++) {
+        cout << "Row " << i + 1 << ": ";
+        for (int j = 0; j < N; j++) {
+            cin >> matrix[i][j];
+        }
+    }
 }
 
+void findLocalMax(const double matrix[][N], bool result[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i == 0 || i == N - 1 || j == 0 || j == N - 1) {
+                result[i][j] = false;
+            } else {
+                if (matrix[i][j] >= matrix[i - 1][j] &&
+                    matrix[i][j] >= matrix[i + 1][j] &&
+                    matrix[i][j] >= matrix[i][j - 1] &&
+                    matrix[i][j] >= matrix[i][j + 1]) {
+                    result[i][j] = true;
+                } else {
+                    result[i][j] = false;
+                }
+            }
+        }
+    }
+}
+
+void showMatrix(const bool matrix[][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    double A[N][N];
+    bool B[N][N];
+    cout << "Input the matrix.....\n";
+    inputMatrix(A);
+    findLocalMax(A, B);
+    cout << "Result is \n";
+    showMatrix(B);
+    return 0;
+}
 //Do not modify source code above this line 
